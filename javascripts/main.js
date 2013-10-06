cfb4f8abd17626e5a6741f3cfdd2befd328d3d52
Jony Zongchen Fu <zongchen.fu@gmail.com>
2013-10-06 02:59:10 -0400

    Updated css code and added input validation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+$(document).ready(function () {
+        $('#main_content').headsmart()
+		var elements = document.getElementsByTagName("INPUT");
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].oninvalid = function(e) {
+            e.target.setCustomValidity("");
+            if (!e.target.validity.valid) {
+                e.target.setCustomValidity("Man, who are you?");
+            }
+        };
+        elements[i].oninput = function(e) {
+            e.target.setCustomValidity("");
+        };
+    }
+       
+      }
+	  
+	  )
